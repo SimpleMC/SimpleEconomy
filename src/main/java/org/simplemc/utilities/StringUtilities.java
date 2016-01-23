e67592d15ecf8cac6649570904e3ffb8dacce55a
Benjamin Harrison-Sims <ben@drunkscience.co.uk>
2016-01-23 20:59:30 +0000

    Add jdoc to StringUtilities.formatDouble

--- src/main/java/org/simplemc/utilities/StringUtilities.java
+++ src/main/java/org/simplemc/utilities/StringUtilities.java
@@ -5,6 +5,12 @@ import java.text.NumberFormat;
 
 public class StringUtilities
 {
+    /**
+     * Formats a double to 2dp
+     * Example: 10.00000 -> 10.00
+     * @param d The double to format
+     * @return Formatted double as a string.
+     */
     public static String formatDouble(Double d)
     {
         NumberFormat formatter = new DecimalFormat("#0.00");
